8248c86abc6890dbf34fbbf378acbe42cf53f8bc
GuDemori <gusavo.demorii@gmail.com>
2025-06-29 14:46:39 -0300

    solving bug

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -44,14 +44,14 @@ class SupplierController extends Controller
 
     public function update(UpdateSupplierRequest $request, int $id): JsonResponse
     {
-        $dto = new SupplierDTO(...$request->only([
-            'name',
-            'company_name',
-            'email',
-            'phone',
-            'document',
-            'city',
-        ]));
+        $dto = new SupplierDTO(
+            name: $request->input('name'),
+            companyName: $request->input('company_name'),
+            email: $request->input('email'),
+            phone: $request->input('phone'),
+            document: $request->input('document'),
+            city: $request->input('city'),
+        );
 
         $supplier = $this->service->update($id, $dto);
 
